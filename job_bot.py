7302d9c93f8b92dc45e11947aaa66ff28fb1340e
mak-lgs <mayank@localgrownfoods.ca>
2024-04-13 17:45:03 -0700

    Added Recruiter Message

---
+++ job_bot.py
@@ -0,0 +1,81 @@
+from langchain.schema import BaseOutputParser
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (SystemMessagePromptTemplate,HumanMessagePromptTemplate,ChatPromptTemplate)
+from langchain.chains import LLMChain
+from docx import Document
+import PyPDF2
+import json
+from io import BytesIO
+import streamlit as st
+import pyperclip
+
+class OutputParser(BaseOutputParser):
+    def parse(self,text:str):
+        print(text)
+        ret=json.loads(text.strip())
+        for i in ret.keys():
+            ret[i]=ret[i].replace("\\n","\n")
+        return ret
+class JobBot():
+    def __init__(self):
+        self.resume=""
+    def parse_resume(self,pdf_file):
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            self.resume += page.extract_text()
+    
+    class CoverLetterBot():
+        def __init__(self,name,job_bot):
+            self.job_bot=job_bot
+            chat_model=ChatOpenAI(model_name=name,api_key=st.secrets.OPENAI_API_KEY)
+            system_template=open('Prompts/prompt_cover_letter.txt', 'r').read()
+            human_template="Resume:{resume}\nJob Description:{job_description}"
+            system_message_prompt=SystemMessagePromptTemplate.from_template(system_template)
+            human_message_prompt=HumanMessagePromptTemplate.from_template(human_template)
+            chat_prompt=ChatPromptTemplate.from_messages([system_message_prompt,human_message_prompt])
+            self.chain=LLMChain(
+                                llm=chat_model,
+                                prompt=chat_prompt,
+                                output_parser=OutputParser()
+                                )
+            self.bot_response=""
+            self.cover_letter=""
+            self.file_name=""
+        def generate_cover_letter(self,job_description):
+            self.bot_response=self.chain.run(resume=self.job_bot.resume,job_description=job_description)
+            self.cover_letter=self.bot_response.get("Cover Letter")
+            self.file_name=f'Cover Letter-{self.bot_response.get("Job Title")}.docx'
+            # print(self.file_name)
+        def create_word_doc(self):
+            document = Document()
+            # print(self.cover_letter)
+            paragraphs = self.cover_letter.split('\n')
+            for paragraph in paragraphs:
+                document.add_paragraph(paragraph)
+            doc_buffer = BytesIO()
+            document.save(doc_buffer)
+            doc_buffer.seek(0)
+            return doc_buffer
+
+    class RecruiterMessage():
+        def __init__(self,name,job_bot):
+            self.job_bot=job_bot
+            chat_model=ChatOpenAI(model_name=name,api_key=st.secrets.OPENAI_API_KEY)
+            system_template=open('Prompts/prompt_recruiter_message.txt', 'r').read()
+            human_template="Resume:{resume}\nContext/Job Description:{context}"
+            system_message_prompt=SystemMessagePromptTemplate.from_template(system_template)
+            human_message_prompt=HumanMessagePromptTemplate.from_template(human_template)
+            chat_prompt=ChatPromptTemplate.from_messages([system_message_prompt,human_message_prompt])
+            self.chain=LLMChain(
+                                llm=chat_model,
+                                prompt=chat_prompt,
+                                output_parser=OutputParser()
+                                )
+            self.context=""
+            self.message=""
+            self.bot_response=""
+        def generate_message(self,context):
+            self.context=context
+            self.bot_response=self.chain.run(resume=self.job_bot.resume,context=self.context)
+            self.message=self.bot_response["message"]
--- main.py
+++ main.py
@@ -1,62 +1,90 @@
 import streamlit as st
-from cover_letter_bot import CoverLetterBot
+from job_bot import JobBot
+import pyperclip
 
-bot3=CoverLetterBot("gpt-3.5-turbo")
-bot4=CoverLetterBot("gpt-4-turbo")
 if not st.session_state.keys():
-    st.session_state["gpt3"]=""
-    st.session_state["gpt4"]=""
+    st.session_state.job_bot=JobBot()
+    for i in st.session_state.keys():
+        print(i)
 
+
+def copy_to_clipboard(text):
+    pyperclip.copy(text)
+    st.success("Copied to clipboard!")
 
 def upload_pdf():
     st.title("Upload Resume in PDF")
     uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
     if uploaded_file:
-        bot3.parse_resume(uploaded_file)
-        bot4.parse_resume(uploaded_file)
+        st.session_state.job_bot.parse_resume(uploaded_file)
         st.success("PDF file uploaded successfully!")
-
-def gpt3():
-    st.write(st.session_state["gpt3"])
+    st.header("Parsed Resume:")
+    st.write(st.session_state.job_bot.resume)
+def cover_letter_gpt3():
+    st.title("Cover Letter GPT3")
+    st.header("Enter Job Description:")
     prompt = st.chat_input("Enter Only The Job Description Here")
     if prompt:
         st.write(f"Job: {prompt}")
-        bot3.generate_cover_letter(prompt)
-        st.write(f"Cover Letter:{bot3.cover_letter}")
-        st.session_state["gpt3"]+=f"Cover Letter:{bot3.cover_letter}"
-        doc_buffer=bot3.create_word_doc()
+        st.session_state.cover_letter_gpt3.generate_cover_letter(prompt)
+        st.header("Cover Letter:")
+        st.write(st.session_state.cover_letter_gpt3.cover_letter)
+        doc_buffer=st.session_state.cover_letter_gpt3.create_word_doc()
         st.download_button(
         label="Download Cover Letter",
         data=doc_buffer,
-        file_name=bot3.file_name,
+        file_name=st.session_state.cover_letter_gpt3.file_name,
         mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
     )
 
-def gpt4():
-    st.write(st.session_state["gpt4"])
+def cover_letter_gpt4():
+    st.title("Cover Letter GPT4")
+    st.header("Enter Job Description:")
     prompt = st.chat_input("Enter Only The Job Description Here")
     if prompt:
         st.write(f"Job: {prompt}")
-        bot4.generate_cover_letter(prompt)
-        st.write(f"Cover Letter:{bot4.cover_letter}")
-        st.session_state["gpt4"]+=f"Cover Letter:{bot4.cover_letter}"
-        doc_buffer=bot4.create_word_doc()
+        st.session_state.cover_letter_gpt4.generate_cover_letter(prompt)
+        st.header("Cover Letter:")
+        st.write(st.session_state.cover_letter_gpt4.cover_letter)
+        doc_buffer=st.session_state.cover_letter_gpt4.create_word_doc()
         st.download_button(
         label="Download Cover Letter",
         data=doc_buffer,
-        file_name=bot4.file_name,
+        file_name=st.session_state.cover_letter_gpt4.file_name,
         mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
     )
+def recruiter_message():
+    st.title("Recruiter Message")
+    st.header("Bot:")
+    st.write("Give context for message or paste job description")
+    prompt = st.chat_input("Enter Here")
+    if prompt:
+        st.header("User:")
+        st.write(prompt)
+        st.session_state.recruiter_message_gpt4.generate_message(prompt)
+        st.header("Bot:")
+        st.code(f"{st.session_state.recruiter_message_gpt4.message}",language="text")
+        
 def main():
     st.sidebar.title('Navigation')
-    selected = st.sidebar.radio('Go to', ["Upload Resume","GPT3.5","GPT4"])
+    selected = st.sidebar.radio('Go to', ["Upload Resume","Cover Letter - GPT3.5","Cover Letter - GPT4","Recruiter Message - GPT4"])
+    for i in st.session_state.keys():
+        print(i)
     if selected=="Upload Resume":
         upload_pdf()
-    elif selected=="GPT3.5":
-        gpt3()
-    elif selected=="GPT4":
-        gpt4()
+    elif selected=="Cover Letter - GPT3.5":
+        if 'cover_letter_gpt3' not in st.session_state.keys():
+            st.session_state.cover_letter_gpt3=st.session_state.job_bot.CoverLetterBot("gpt-3.5-turbo",st.session_state.job_bot)
+        cover_letter_gpt3()
+    elif selected=="Cover Letter - GPT4":
+        if 'cover_letter_gpt4' not in st.session_state.keys():
+            st.session_state.cover_letter_gpt4=st.session_state.job_bot.CoverLetterBot("gpt-4-turbo",st.session_state.job_bot)
+        cover_letter_gpt4()
+    elif selected=="Recruiter Message - GPT4":
+        if 'recruiter_message_gpt4' not in st.session_state.keys():
+            st.session_state.recruiter_message_gpt4=st.session_state.job_bot.RecruiterMessage("gpt-4-turbo",st.session_state.job_bot)
+        recruiter_message()
 
 
 if __name__ == "__main__":
