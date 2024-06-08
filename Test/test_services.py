505bf1e99bc0890f1b712413add792f0bce6968f
mak-lgs <mayank@localgrownfoods.ca>
2024-06-08 12:05:04 -0700

    Fixed Unit Testing

---
+++ Test/test_services.py
@@ -0,0 +1,19 @@
+import pytest
+from io import BytesIO
+import sys
+sys.path.insert(0,"../")
+from bot_services.job_bot import JobBot
+
+def test_parse_resume():
+    job_bot = JobBot()
+    with open('sample_resume.pdf', 'rb') as f:
+        job_bot.parse_resume(BytesIO(f.read()))
+    assert job_bot.resume != ""
+
+def test_generate_cover_letter():
+    job_bot = JobBot()
+    job_bot.parse_resume(open('sample_resume.pdf', 'rb'))
+    cover_letter_bot = JobBot.CoverLetterBot('gpt-3.5-turbo', job_bot)
+    cover_letter_bot.generate_cover_letter('sample job description')
+    assert cover_letter_bot.cover_letter != ""
+    assert cover_letter_bot.file_name.endswith('.docx')
